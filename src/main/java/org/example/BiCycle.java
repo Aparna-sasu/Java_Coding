fc5718eb74c453a0be001c19d89910aa402d1c0f
Aparna AnandaPadmanabhan <aparna.anadan11@gmail.com>
2024-01-25 17:14:05 -0800

    Initial Commit

---
+++ src/main/java/org/example/BiCycle.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class BiCycle {
+
+    String define_me(){
+        return "a vehicle with pedals.";
+    }
+}
---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        MotorCycle m = new MotorCycle();
+    }
+}
